321db91ff06af1d79b2aebf2049c426eef7c78aa
Abdullahi Kabri <kabriacid01@gmail.com>
2024-07-28 12:14:47 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/upload', [UserController::class, 'create']);
-
-Route::post('/upload', [UserController::class, 'store']);
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
